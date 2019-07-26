f787a18a251f88cd5e3e0378abe38c0d80c3c248
gnkhandsome <1456448257@qq.com>
2019-07-26 16:55:36 +0800

    refactor (all)  Optimizing layout and formatting code

--- BCX_Wallet/module_mine/src/main/java/com/cocos/module_mine/asset_operate/sale_nhasset/SaleNHAssetViewModel.java
+++ BCX_Wallet/module_mine/src/main/java/com/cocos/module_mine/asset_operate/sale_nhasset/SaleNHAssetViewModel.java
@@ -3,12 +3,9 @@ package com.cocos.module_mine.asset_operate.sale_nhasset;
 import android.app.Application;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
-import android.databinding.ObservableInt;
 import android.support.annotation.NonNull;
-import android.view.View;
 
 import com.cocos.library_base.base.BaseViewModel;
-import com.cocos.library_base.binding.command.BindingAction;
 import com.cocos.library_base.binding.command.BindingCommand;
 import com.cocos.module_mine.entity.NHAssetModel;
 
@@ -35,26 +32,11 @@ public class SaleNHAssetViewModel extends BaseViewModel {
         public ObservableBoolean choosePriceSymbolObservable = new ObservableBoolean(false);
     }
 
-    public BindingCommand backOnClickCommand = new BindingCommand(new BindingAction() {
-        @Override
-        public void call() {
-            finish();
-        }
-    });
+    public BindingCommand backOnClickCommand = new BindingCommand(() -> finish());
 
-    public BindingCommand choosePriceSymbolCommand = new BindingCommand(new BindingAction() {
-        @Override
-        public void call() {
-            uc.choosePriceSymbolObservable.set(!uc.choosePriceSymbolObservable.get());
-        }
-    });
+    public BindingCommand choosePriceSymbolCommand = new BindingCommand(() -> uc.choosePriceSymbolObservable.set(!uc.choosePriceSymbolObservable.get()));
 
-    public BindingCommand saleNHNextOnClickCommand = new BindingCommand(new BindingAction() {
-        @Override
-        public void call() {
-            uc.saleNHNBtnObservable.set(!uc.saleNHNBtnObservable.get());
-        }
-    });
+    public BindingCommand saleNHNextOnClickCommand = new BindingCommand(() -> uc.saleNHNBtnObservable.set(!uc.saleNHNBtnObservable.get()));
 
     public void setNhAssetId(NHAssetModel.NHAssetModelBean nhAssetModelBean) {
         nhAssetId.set(nhAssetModelBean.id);
