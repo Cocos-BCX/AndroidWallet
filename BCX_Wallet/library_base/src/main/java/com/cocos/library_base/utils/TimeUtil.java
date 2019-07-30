7cdfac722285a8e1a3ee662ff60ff8d916aa525c
gnkhandsome <1456448257@qq.com>
2019-07-30 19:33:24 +0800

    fix （TimeUtil） fix time with now time zone

--- BCX_Wallet/library_base/src/main/java/com/cocos/library_base/utils/TimeUtil.java
+++ BCX_Wallet/library_base/src/main/java/com/cocos/library_base/utils/TimeUtil.java
@@ -1,8 +1,15 @@
 package com.cocos.library_base.utils;
 
+import android.text.BidiFormatter;
+import android.text.TextDirectionHeuristics;
+import android.text.TextUtils;
+import android.view.View;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * @author ningkang.guo
@@ -22,11 +29,39 @@ public class TimeUtil {
             }
             Calendar ca = Calendar.getInstance();
             ca.setTime(value);
-            ca.add(Calendar.HOUR_OF_DAY, 8);
+            ca.add(Calendar.HOUR_OF_DAY, getTimeZone());
             newValue = f.format(ca.getTime());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return newValue;
+    }
+
+    public static int getTimeZone() {
+        Calendar mDummyDate;
+        mDummyDate = Calendar.getInstance();
+        Calendar now = Calendar.getInstance();
+        mDummyDate.setTimeZone(now.getTimeZone());
+        mDummyDate.set(now.get(Calendar.YEAR), 11, 31, 13, 0, 0);
+        return now.getTimeZone().getRawOffset() / 1000 / 60 / 60;
+    }
+
+    public static String getTimeZoneText(TimeZone tz, boolean includeName) {
+        Date now = new Date();
+
+        SimpleDateFormat gmtFormatter = new SimpleDateFormat("ZZZZ");
+        gmtFormatter.setTimeZone(tz);
+        String gmtString = gmtFormatter.format(now);
+        BidiFormatter bidiFormatter = BidiFormatter.getInstance();
+        Locale l = Locale.getDefault();
+        boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
+        gmtString = bidiFormatter.unicodeWrap(gmtString,
+                isRtl ? TextDirectionHeuristics.RTL : TextDirectionHeuristics.LTR);
+
+        if (!includeName) {
+            return gmtString;
+        }
+
+        return gmtString;
     }
 }
